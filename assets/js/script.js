6572cfc54d4f3997a26c14b4b1598511ec7fab67
Adam Daly <you@example.com>
2024-04-11 15:14:14 +0100

    Setup answerBoxClicked function, add necessary dom elements and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
+// get necessary DOM elements
+const categoryInfo = document.getElementById("category-info");
+
+// get the category names from data in questions.js
+const categoryNames = Object.keys(data);
+
+// variables that need to retain state between functions
+let isShowingCategories = true;
+let currentCategory;
+let numRounds = 0;
+
+
+// set the category if categories are showing, otherwise check answer
+function answerBoxClicked(index) {
+    //console.log(isShowingCategories);
+    if (isShowingCategories === true) {
+        currentCategory = index;
+        categoryInfo.innerHTML = `Current Category: ${categoryNames[currentCategory]}`;
+        fetchQuestionFromCategory(currentCategory);
+        isShowingCategories = false;
+        numRounds++;
+    } else {
+        submitAnswer(index);
+    }
+}
+
+
 // Get a question from a specific category and set up the content
 function fetchQuestionFromCategory(category) {
-
+    console.log(category);
 }
 
 // Check the answer, update score and get the next question
 function submitAnswer(answerIndex) {
-}
-
-// set the category if categories are showing, otherwise check answer
-function answerBoxClicked(index) {
+    console.log(answerIndex);
 }
 
 // Display stats and allow a category change
