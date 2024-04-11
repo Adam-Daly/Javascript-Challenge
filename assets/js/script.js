6764d4390a9d710c8e60cb94e82bc0719cf80d76
Adam Daly <you@example.com>
2024-04-11 11:34:04 +0100

    Setup initial javascript file, describe main functions needed

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Get a question from a specific category and set up the content
+function fetchQuestionFromCategory(category) {
+
+}
+
+// Check the answer, update score and get the next question
+function submitAnswer(answerIndex) {
+}
+
+// set the category if categories are showing, otherwise check answer
+function answerBoxClicked(index) {
+}
+
+// Display stats and allow a category change
+function categoryChange() {
+
+}
+
+// Reset all values back to default and show welcome text
+function restartChallenge() {
+
+}
