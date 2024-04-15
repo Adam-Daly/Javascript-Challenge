8444b7f686d390d4752f3b931cbbb14d746ae201
Adam Daly <you@example.com>
2024-04-15 11:25:59 +0100

    Remove console logs and add comments throughout the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,14 +26,12 @@ let seenQuestions = [];
 
 // Get a question from a specific category and set up the content
 function fetchQuestionFromCategory(category) {
-    //console.log("data = ", data);
 
+    // Get the category from data in questions.js
     const questions = data[categoryNames[category]];
 
-    //console.log("questions = ", questions);
-
     let randomIndex;
-    
+
     // Choose a random question and make sure we havent seen it yet
     do {
         // Choose a random question index, expected output is between 0 to questions.length - 1
@@ -42,22 +40,25 @@ function fetchQuestionFromCategory(category) {
     while (seenQuestions.includes(randomIndex));
 
     seenQuestions.push(randomIndex);
-
-    //console.log("randomIndex = ", randomIndex);
     
+    // Get the random question and increment questionNumber
     const randomQuestion = questions[randomIndex];
     questionNumber += 1;
-
+    
+    // Display the random question in the HTML
     quizQuestion.innerHTML = `<h2>Question ${questionNumber}: ${randomQuestion.question}</h2>`;
 
+    // Populate the answers
     for (let i = 0; i < answerArray.length; i++) {
         answerArray[i].innerHTML = `${randomQuestion.answers[i]}`;
     }
 
+    // Remove any correctness classes
     for (let i = 0; i < answers.length; i++) {
         answers[i].classList.remove("correct", "incorrect");
     }
 
+    // set correct answer for answer validating
     correctAnswer = randomQuestion.correctAnswer;
 
     //set mascot image to neutral
@@ -67,11 +68,12 @@ function fetchQuestionFromCategory(category) {
 
 // Check the answer, update score and get the next question
 function submitAnswer(answerIndex) {
-    //console.log(answers.length);
 
     for (let i = 0; i < answers.length; i++) {
+        // Remove existing correctness classes
         answers[i].classList.remove("correct", "incorrect");
 
+        // Add the appropriate correctness class
         if (correctAnswer === answers[i].querySelector('p').innerHTML) {
             answers[i].classList.add("correct");
         } else {
@@ -79,6 +81,7 @@ function submitAnswer(answerIndex) {
         }
     }
 
+    // Increment relevant total, update score text, update mascot 
     if (answerArray[answerIndex].innerHTML === correctAnswer) {
         correctTotal++;
         correctScore.innerHTML = `Correct: ${correctTotal}`;
@@ -89,14 +92,12 @@ function submitAnswer(answerIndex) {
         mascot.src = "assets/images/mascot-incorrect.png";
     }
 
-    //console.log(correctAnswer);
-
     // remove onclick attribute to prevent spam clicking during wait
     for (let i = 0; i < answers.length; i++) {
         answers[i].removeAttribute("onclick");
     }
 
-    // wait for 2 seconds to show correct answer
+    // wait for 2 seconds to show correct answer and mascot reaction
     // then enable clicking again
     sleep(500)
         .then(() => {
@@ -116,7 +117,6 @@ function submitAnswer(answerIndex) {
 
 // set the category if categories are showing, otherwise check answer
 function answerBoxClicked(index) {
-    //console.log(isShowingCategories);
     if (isShowingCategories === true) {
         currentCategory = index;
         categoryInfo.innerHTML = `Current Category: ${categoryNames[currentCategory]}`;
@@ -131,6 +131,7 @@ function answerBoxClicked(index) {
 // Display stats and allow a category change
 function categoryChange() {
 
+    // Calculate correct answer percentage
     let correctPercent;
     let total = correctTotal + incorrectTotal;
     if (total === 0) {
@@ -138,21 +139,25 @@ function categoryChange() {
     }
     correctPercent = (correctTotal / total) * 100;
 
+    // Show some stats between rounds
     quizQuestion.innerHTML = `
         <h2>Pick a New Category Below <br> or Restart for a Better Score!</h2>
         <h3>You have played through ${numRounds} rounds!</h3>
         <h3>Correct Answer Rate: ${correctPercent.toFixed(2)}%</h3>
     `;
 
+    // Remove existing correctness classes
     for (let i = 0; i < answers.length; i++) {
         answers[i].classList.remove("correct", "incorrect");
     }
 
+    // Add category names
     for (let i = 0; i < answerArray.length; i++) {
         answerArray[i].innerHTML = `${categoryNames[i]}`;
     }
     isShowingCategories = true;
 
+    // Reset category info
     categoryInfo.innerHTML = `Pick A Category Above`;
 
     // Reset seen questions
@@ -161,34 +166,42 @@ function categoryChange() {
     // Change mascot to between round image
     mascot.src = "assets/images/mascot-inbetween-rounds.png";
 
+
 }
 
 // Reset all values back to default and show welcome text
 function restartChallenge() {
 
+    // Add default welcome text
     quizQuestion.innerHTML = `
         <h2>Welcome to <br>Javascript Challenge!</h2>
         <h3>Choose a category below to get your first round of 10 questions:</h3>
     `;
 
+    // Remove existing correctness classes
     for (let i = 0; i < answers.length; i++) {
         answers[i].classList.remove("correct", "incorrect");
     }
 
+    // Add category names
     for (let i = 0; i < answerArray.length; i++) {
         answerArray[i].innerHTML = `${categoryNames[i]}`;
     }
     isShowingCategories = true;
 
+    // Reset Scores back to 0
     correctTotal= 0;
     incorrectTotal = 0;
     correctScore.innerHTML = `Correct: ${correctTotal}`;
     incorrectScore.innerHTML = `Incorrect: ${incorrectTotal}`;
 
+    // Reset category info
     categoryInfo.innerHTML = `Pick A Category Above`;
 
+    // Reset question number
     questionNumber = 0;
 
+    // Reset rounds number
     numRounds = 0;
 
     // Set mascot image back to welcome
